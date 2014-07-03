df814e3f22f37a7f953c15efc2ae70d11360f6c2
Kiran Panesar <kiran@mobilexlabs.com>
2014-07-03 14:50:11 -0500

    Added base MXLInstallTracker files from another project

---
+++ MXLInstallTracker/MXLInstallTracker.h
@@ -0,0 +1,24 @@
+//
+//  MXLAnalyticsManager.h
+//  Instalikes
+//
+//  Created by Kiran Panesar on 25/04/2014.
+//  Copyright (c) 2014 MobileX Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MXLInstallTracker : NSObject
+
+@property (strong, nonatomic, readonly) NSString *apiKey;
+@property (strong, nonatomic, readonly) NSString *apiSecret;
+@property (strong, nonatomic, readonly) NSString *build;
+@property (strong, nonatomic, readonly) NSString *adwordsConversionID;
+@property (strong, nonatomic, readonly) NSString *adwordsLabel;
+
++(MXLInstallTracker *)sharedManager;
+
+-(void)setUpWithGAKey:(NSString *)key GAsecret:(NSString *)secret GAbuild:(NSString *)build MATAdID:(NSString *)adID MATConversionKey:(NSString *)conversionKey AWConversionID:(NSString *)awConversion AWLabel:(NSString *)awLabel;
+-(void)appBecameActive;
+
+@end
---
+++ MXLInstallTracker/MXLInstallTracker.m
@@ -0,0 +1,79 @@
+//
+//  MXLAnalyticsManager.m
+//  Instalikes
+//
+//  Created by Kiran Panesar on 25/04/2014.
+//  Copyright (c) 2014 MobileX Labs. All rights reserved.
+//
+
+#import "MXLInstallTracker.h"
+
+#import <GA-iOS-SDK/GameAnalytics.h>
+#import <MobileAppTracker/MobileAppTracker.h>
+#import <AdSupport/AdSupport.h>
+#import "ACTReporter.h"
+
+#import "PDKeychainBindings.h"
+
+@interface MXLInstallTracker ()
+
+@property (strong, nonatomic, readwrite) NSString *apiKey;
+@property (strong, nonatomic, readwrite) NSString *apiSecret;
+@property (strong, nonatomic, readwrite) NSString *build;
+@property (strong, nonatomic, readwrite) NSString *adwordsConversionID;
+@property (strong, nonatomic, readwrite) NSString *adwordsLabel;
+
+@end
+
+@implementation MXLInstallTracker
+
++(MXLInstallTracker *)sharedManager {
+    static MXLInstallTracker *sharedManager = nil;
+    if (!sharedManager) {
+        sharedManager = [[super allocWithZone:nil] init];
+    }
+    return sharedManager;
+}
+
++(id)allocWithZone:(NSZone *)zone {
+    return [self sharedManager];
+}
+
+-(void)setUpWithGAKey:(NSString *)key GAsecret:(NSString *)secret GAbuild:(NSString *)build MATAdID:(NSString *)adID MATConversionKey:(NSString *)conversionKey AWConversionID:(NSString *)awConversion AWLabel:(NSString *)awLabel{
+    _apiKey              = key;
+    _apiSecret           = secret;
+    _build               = build;
+    _adwordsConversionID = awConversion;
+    _adwordsLabel        = awLabel;
+    
+    [GameAnalytics setGameKey:_apiKey
+                    secretKey:_apiSecret
+                        build:_build];
+    
+    [GameAnalytics updateSessionID];
+    
+    [MobileAppTracker initializeWithMATAdvertiserId:adID
+                                   MATConversionKey:conversionKey];
+    
+    [MobileAppTracker setAppleAdvertisingIdentifier:[[ASIdentifierManager sharedManager] advertisingIdentifier]
+                         advertisingTrackingEnabled:[[ASIdentifierManager sharedManager] isAdvertisingTrackingEnabled]];
+    
+#if !RELEASE
+    [MobileAppTracker setDebugMode:YES];
+#endif
+    
+    [MobileAppTracker setUserId:[GameAnalytics getUserID]];
+    [MobileAppTracker measureSession];
+    
+    
+    [ACTConversionReporter reportWithConversionID:_adwordsConversionID
+                                            label:_adwordsLabel
+                                            value:@"0.000000"
+                                     isRepeatable:NO];
+}
+
+-(void)appBecameActive {
+    [MobileAppTracker measureSession];
+}
+
+@end
